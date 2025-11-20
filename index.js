bf4dc6b2a578f9d75240a24434c2df8efff63c13
onlinewithbeta <onlinewithbeta@gmail.com>
2025-11-20 16:17:25 +0100

    env¹

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("start");
+import express from "express"
+
+const app = express();
+
+
+
+app.use((req,res)=>{
+  console.log("active")
+  res.status(200).json({message:"avaliable"})
+})
+
+const PORT = process.env.PORT||2025;
+
+app.listen(PORT, ()=>{
+  console.log(`http://localhost:${PORT}`)
+})
